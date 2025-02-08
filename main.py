82049d7d07beb2f6ec47188664f22500233784ff
blapeyrouse <blapeyrouse@gmail.com>
2025-02-07 22:47:54 -0700

    updated main.py to count the number of words in the file

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    with open("books/frankenstein.txt") as f:   # opens specified path and reads the contents of the .txt file
         file_contents = f.read()
+    
+    # print(file_contents)
 
-    print(file_contents)
+    count = 0
+    words = file_contents.split()   # splits file_contents into individual words
+    for word in words:  # iterates through each word and adds 1 to total count 
+        count += 1
+
+    print(count)
+    return count
+
 
 main()
 
