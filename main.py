02dfc2a7714a02d7586da9d710275ab7529bab4c
blapeyrouse <blapeyrouse@gmail.com>
2025-02-11 21:34:17 -0700

    updated main.py to separate out count_words function

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 def main():
     with open("books/frankenstein.txt") as f:   # opens specified path and reads the contents of the .txt file
         file_contents = f.read()
+    # print(file_contents)    
+    print(count_words(file_contents))
     
-    # print(file_contents)
 
+def count_words(file_contents):
     count = 0
     words = file_contents.split()   # splits file_contents into individual words
     for word in words:  # iterates through each word and adds 1 to total count 
         count += 1
 
-    print(count)
     return count
-
 
 main()
 
