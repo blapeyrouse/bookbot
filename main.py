0b6e8a2ddfd99337696469b35595c9e5f93d4f3d
blapeyrouse <blapeyrouse@gmail.com>
2025-02-12 21:18:21 -0700

    added make_report function and some formatting in main() to format the report

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 def main():
-    with open("books/frankenstein.txt") as f:   # opens specified path and reads the contents of the .txt file
+    path = "books/frankenstein.txt"
+    with open(path) as f:   # opens specified path and reads the contents of the .txt file
         file_contents = f.read()  
-    print(count_words(file_contents))    # prints word count in main
-    # print(count_characters(file_contents))    # prints dictionary of characters, including spaces and symbols, and the number of times it appears
-    print(make_list_of_dicts(file_contents))
-    print("NOW LETS SORT IT")
-    print(sort_list(file_contents))
+    count = count_words(file_contents)
+    print(f"--- Begin report of {path} ---")
+    print(f"{count} words found in the document \n")
+    print(make_report(file_contents))
+    print(f"--- End report ---")
 
 
 def count_words(file_contents):    # function to count words
@@ -29,7 +30,8 @@ def count_characters(file_contents):    # function to count total number of char
     return character_counts
 
 
-def sort_on(dict):
+def sort_on(dict):    # defines which key to sort on
+
     return dict["num"]
     
 
@@ -46,17 +48,19 @@ def make_list_of_dicts(file_contents):    # function to convert the dictionary o
     return letters
 
 
-def sort_list(file_contents):
+def sort_list(file_contents):    # sorts the list by the 'num' key and reverses it so that the letter that occurs the most often is first
     list = make_list_of_dicts(file_contents)
     list.sort(reverse=True, key=sort_on)
 
     return list
 
-
-
-
-# def make_report(file_contents):
+def make_report(file_contents):    # iterates through sorted list and adds a string for each character and it's count
+    report = []
+    sorted_list = sort_list(file_contents)
+    for letter in sorted_list:
+        report.append(f"The '{letter['name']}' character was found {letter['num']} times")
     
+    return "\n".join(report)    # returns each string on it's own line
 
 
 
